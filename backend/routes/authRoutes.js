11c9bbfe932bb9fc723ee6f6d2d0f57e4b82ad2e
Roshni Singh <roshnisingh1228@gmail.com>
2025-03-23 00:24:22 +0530

    backend structure

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/authController');
-
-// User Authentication Routes
-router.post('/register', registerUser); // Register a new user
-router.post('/login', loginUser); // User login
-
-module.exports = router;
